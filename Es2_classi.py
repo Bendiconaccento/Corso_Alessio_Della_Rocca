ce7295cce9f93f62023f5a57253979d3fcf060f4
Bendiconaccento <alessiodellarocca@gmail.com>
2024-09-11 11:22:24 +0200

    Add files via upload

---
+++ Es2_classi.py
@@ -0,0 +1,15 @@
+
+class Libro:
+
+    def __init__ (self, titolo, autore, pagine):    # Inizializza i tre attributi
+
+        self.titolo = titolo
+        self.autore = autore
+        self.pagine = pagine
+
+    def descrizione(self):                          # Restituisce la descrizione del libro
+
+        return f"Il libro {self.titolo} è stato scritto da {self.autore} e ha {self.pagine} pagine"
+    
+libro = Libro("Il Gattopardo", "Tomasi Di Lampedusa", 299)
+print(libro.descrizione())                          # Printa la descrizione del libro
