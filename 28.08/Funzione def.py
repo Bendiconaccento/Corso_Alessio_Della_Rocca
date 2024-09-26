f635931b11a27424cfe508efe7c03da01094c81d
Bendiconaccento <alessiodellarocca@gmail.com>
2024-09-26 16:05:11 +0200

    Add files via upload

---
+++ 28.08/Funzione def.py
@@ -0,0 +1,4 @@
+def saluta(nome):               # def = crei una nuova funzione
+    print(f"Ciao, {nome}!")     # f lo devi mettere sennò ti print "Ciao, {nome}!"
+
+saluta("Mario")
---
+++ 28.08/While.py
@@ -0,0 +1,5 @@
+conteggio = 0
+
+while conteggio <5:   #il conteggio continua fino a che non si raggiunge 5
+    print(conteggio)
+    conteggio += 1
