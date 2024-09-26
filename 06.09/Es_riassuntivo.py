b43cfbcb9bfc30523de07ab846849e1c19c64a3b
Bendiconaccento <alessiodellarocca@gmail.com>
2024-09-26 16:02:45 +0200

    Add files via upload

---
+++ 06.09/Es_riassuntivo.py
@@ -0,0 +1,29 @@
+
+def argomento_1():
+    print("Argomento 1: UML")
+
+def argomento_2():
+    print("Argomento 2: Metodologie")
+
+def argomento_3():
+    print("Argomento 3: Tasking")
+
+def argomento_4():
+    print("Argomento 4: IDE")
+
+def argomento_5():
+    print("Argomento 5: Liste")
+
+def argomento_6():
+    print("Argomento 6: Range")
+
+def argomento_7():
+    print("Argomento 7: Cicli")
+
+
+def menu ():
+    argomenti = {
+        1. "Argomento 1: UML"
+        2. "Argomento 2: "
+    }
+    
